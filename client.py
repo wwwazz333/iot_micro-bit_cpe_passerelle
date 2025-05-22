5add731b7fce9a5019bb24e75422bb14f77b361a
wwwazz <virgile.menard@gmail.com>
2025-05-22 17:48:45 +0200

    first commit

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+
+ 
+
+msgFromClient       = "getValues()"
+
+bytesToSend         = msgFromClient.encode()
+
+serverAddressPort   = ("localhost", 10000)
+
+bufferSize          = 1024
+
+ 
+
+# Create a UDP socket at client side
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+ 
+
+# Send to server using created UDP socket
+
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+ 
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+ 
+
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
---
+++ controller.py
@@ -0,0 +1,125 @@
+# Program to control passerelle between Android application
+# and micro-controller through USB tty
+import time
+import argparse
+import signal
+import sys
+import socket
+import socketserver
+import serial
+import threading
+import json 
+
+
+HOST           = "localhost"  # The server's hostname or IP address²
+UDP_PORT       = 10000
+MICRO_COMMANDS = ["TL" , "LT"]
+FILENAME        = "values.json"
+LAST_VALUE      = ""
+
+DATA_MEASUREMENT = {}
+
+class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request[0].strip().decode()
+        socket = self.request[1]
+        current_thread = threading.current_thread()
+        print("{}: client: {}, wrote: {}".format(current_thread.name, self.client_address, data))
+        if data != "":
+                        if data in MICRO_COMMANDS: # Send message through UART
+                                sendUARTMessage(data)
+                                
+                        elif data == "getValues()": # Sent last value received from micro-controller
+                                socket.sendto(json.dumps(DATA_MEASUREMENT, indent=4).encode(), self.client_address) 
+                                # TODO: Create last_values_received as global variable      
+                        else:
+                                print("Unknown message: ",data)
+
+class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
+    pass
+
+
+# send serial message 
+SERIALPORT = "/dev/ttyACM0"
+BAUDRATE = 115200
+ser = serial.Serial()
+
+def initUART():        
+        # ser = serial.Serial(SERIALPORT, BAUDRATE)
+        ser.port=SERIALPORT
+        ser.baudrate=BAUDRATE
+        ser.bytesize = serial.EIGHTBITS #number of bits per bytes
+        ser.parity = serial.PARITY_NONE #set parity check: no parity
+        ser.stopbits = serial.STOPBITS_ONE #number of stop bits
+        ser.timeout = None          #block read
+
+        # ser.timeout = 0             #non-block read
+        # ser.timeout = 2              #timeout block read
+        ser.xonxoff = False     #disable software flow control
+        ser.rtscts = False     #disable hardware (RTS/CTS) flow control
+        ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
+        #ser.writeTimeout = 0     #timeout for write
+        print('Starting Up Serial Monitor')
+        try:
+                ser.open()
+        except serial.SerialException:
+                print("Serial {} port not available".format(SERIALPORT))
+                exit()
+
+
+
+def sendUARTMessage(msg):
+    ser.write(msg.encode())
+    print("Message <" + msg + "> sent to micro-controller." )
+
+def writeToFile():
+        f= open(FILENAME,"w")
+        f.write(json.dumps(DATA_MEASUREMENT, indent=4))
+        f.close()
+def loadFromFile():
+        f= open(FILENAME,"r")
+        try:
+                DATA_MEASUREMENT = json.loads(f.read())
+        except:
+                print("File not found or empty, creating new file")
+                DATA_MEASUREMENT = {}
+        f.close()
+        return DATA_MEASUREMENT
+
+# Main program logic follows:
+if __name__ == '__main__':
+        initUART()
+        loadFromFile()
+        print ('Press Ctrl-C to quit.')
+
+        server = ThreadedUDPServer((HOST, UDP_PORT), ThreadedUDPRequestHandler)
+
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.daemon = True
+
+        try:
+                server_thread.start()
+                print("Server started at {} port {}".format(HOST, UDP_PORT))
+                while ser.isOpen() : 
+                        # time.sleep(100)
+                        if (ser.inWaiting() > 0): # if incoming bytes are waiting 
+                                data_bytes = ser.read(ser.inWaiting())
+                                data_str = data_bytes.decode()
+                                LAST_VALUE += str(data_str)
+
+                                if '\n' in LAST_VALUE:
+                        
+                                        splited = LAST_VALUE.split("=")
+                                        LAST_VALUE = ""
+                                        if len(splited) != 2:
+                                                print("Invalid data received", splited)
+                                                continue
+
+                                        DATA_MEASUREMENT[splited[0]] = splited[1]
+                                        writeToFile()
+        except (KeyboardInterrupt, SystemExit):
+                server.shutdown()
+                server.server_close()
+                ser.close()
+                exit()
